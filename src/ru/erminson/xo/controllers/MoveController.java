e92a675c12f434c675f5259f7aefbe67ca4bf6b4
Lev <erminson@gmail.com>
2020-04-12 17:05:29 +0300

    MoveController initial

--- src/ru/erminson/xo/controllers/MoveController.java
+++ src/ru/erminson/xo/controllers/MoveController.java
@@ -1,4 +1,19 @@
 package ru.erminson.xo.controllers;
 
+import ru.erminson.xo.models.Field;
+import ru.erminson.xo.models.Figure;
+import ru.erminson.xo.models.exceptions.AlreadyOccupiedException;
+import ru.erminson.xo.models.exceptions.InvalidPointException;
+
+import java.awt.*;
+
 public class MoveController {
+    public void applyFigure(final Field field,
+                            final Figure figure,
+                            final Point point) throws InvalidPointException, AlreadyOccupiedException {
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
+    }
 }
