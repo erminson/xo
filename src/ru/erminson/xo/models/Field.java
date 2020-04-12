ac994a32804106dc639267fe8976e8d562480948
Lev <erminson@gmail.com>
2020-04-12 16:18:18 +0300

    FieldTest added

---
+++ src/ru/erminson/xo/models/Field.java
@@ -0,0 +1,45 @@
+package ru.erminson.xo.models;
+
+import ru.erminson.xo.models.exceptions.AlreadyOccupiedException;
+import ru.erminson.xo.models.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class Field {
+    private static final int FIELD_SIZE = 3;
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
+    private final Figure[][] figures = new Figure[FIELD_SIZE][FIELD_SIZE];
+
+    public int getSize() {
+        return FIELD_SIZE;
+    }
+
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if (!isValidPoint(point)) {
+            throw new InvalidPointException();
+        }
+
+        return figures[point.x][point.y];
+    }
+
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if (!isValidPoint(point)) {
+            throw new InvalidPointException();
+        }
+
+        if (figures[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+        }
+
+        figures[point.x][point.y] = figure;
+    }
+
+    private boolean isValidPoint(final Point point) {
+        return isValidCoordinate(point.x) && isValidCoordinate(point.y);
+    }
+
+    private boolean isValidCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    }
+}
---
+++ tst/ru/erminson/xo/models/FieldTest.java
@@ -0,0 +1,88 @@
+package ru.erminson.xo.models;
+
+import org.junit.jupiter.api.Test;
+import ru.erminson.xo.models.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldTest {
+
+    @Test
+    void getSize() {
+        final Field field = new Field();
+        assertEquals(3, field.getSize());
+    }
+
+    @Test
+    void getFigure() {
+    }
+
+    @Test
+    void setFigure() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+        final Figure expectedFigure = inputFigure;
+
+        field.setFigure(inputPoint, inputFigure);
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertEquals(expectedFigure, actualFigure);
+    }
+
+    @Test
+    void testGetFigureWhenFigureNotSet() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    void testGetFigureWhenXIsLessMinValue() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-3,0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void testGetFigureWhenYIsLessMinValue() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,-2);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void testGetFigureWhenXIsMoreMaxValue() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSize(),0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void testGetFigureWhenYIsMoreMaxValue() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,field.getSize() + 1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+}
