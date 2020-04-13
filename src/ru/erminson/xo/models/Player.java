be93d9d826b5d7221777422a51549caaad4cd996
Lev <erminson@gmail.com>
2020-04-13 20:24:23 +0300

    Player - changed String to Player

--- src/ru/erminson/xo/models/Player.java
+++ src/ru/erminson/xo/models/Player.java
@@ -4,7 +4,7 @@ public class Player {
     private final String name;
     private final Figure figure;
 
-    Player(final String name, final Figure figure) {
+    public Player(final String name, final Figure figure) {
         this.name = name;
         this.figure = figure;
     }
