2743c7a0d1a187b7272c9e6d08868c3e4201a5d4
Lev <erminson@gmail.com>
2020-04-13 20:24:55 +0300

    ConsoleView - created

--- src/ru/erminson/xo/views/ConsoleView.java
+++ src/ru/erminson/xo/views/ConsoleView.java
@@ -1,20 +1,90 @@
 package ru.erminson.xo.views;
 
+import ru.erminson.xo.controllers.CurrentMoveController;
+import ru.erminson.xo.controllers.MoveController;
+import ru.erminson.xo.controllers.WinnerController;
+import ru.erminson.xo.models.Field;
+import ru.erminson.xo.models.Figure;
 import ru.erminson.xo.models.Game;
+import ru.erminson.xo.models.exceptions.AlreadyOccupiedException;
+import ru.erminson.xo.models.exceptions.InvalidPointException;
 
+import java.awt.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ConsoleView {
-    protected final Game game;
+    private final CurrentMoveController currentMoveController = new CurrentMoveController();
+    private final WinnerController winnerController = new WinnerController();
+    private final MoveController moveController = new MoveController();
 
-    public ConsoleView(final  Game game) {
-        this.game = game;
+    public void show(final Game game) {
+        System.out.format("Game name: %s\n", game.getName());
+        final Field field = game.getField();
+        for (int x = 0; x < field.getSize(); x++) {
+            if (x != 0)
+                printSeparator();
+            printLine(field, x);
+        }
     }
 
-    public void showGameName() {
-//        System.out.println(game.getGameName());
+    public boolean move(final Game game) {
+        final Field field = game.getField();
+        final Figure winner = winnerController.getWinner(field);
+        if (winner != null) {
+            System.out.format("Winner is: %s\n", winner);
+            return false;
+        }
+        final Figure currentFigure = currentMoveController.currentMove(field);
+        if (currentFigure == null) {
+            System.out.println("No winner and no moves left!");
+            return false;
+        }
+        System.out.format("Please enter move point for: %s\n", currentFigure);
+        final Point point = askPoint();
+        try {
+            moveController.applyFigure(field, currentFigure, point);
+        } catch (final InvalidPointException | AlreadyOccupiedException e) {
+            System.out.println("Point is invalid!");
+        }
+        return true;
     }
 
-    public void showPlayersName() {
-        System.out.println(game.getPlayers());
+    private Point askPoint() {
+        return new Point(askCoordinate("X") - 1, askCoordinate("Y") - 1);
+    }
+
+    private int askCoordinate(final String coordinateName) {
+        System.out.format("Please input %s:", coordinateName);
+        final Scanner in = new Scanner(System.in);
+        try {
+            return in.nextInt();
+        } catch (final InputMismatchException e) {
+            System.out.println("0_0 olololo!!!!!");
+            return askCoordinate(coordinateName);
+        }
+    }
+
+    private void printLine(final Field field,
+                           final int x) {
+        for (int y = 0; y < field.getSize(); y++) {
+            if (y != 0)
+                System.out.print("|");
+            System.out.print(" ");
+            final Figure figure;
+            try {
+                figure = field.getFigure(new Point(y, x));
+            } catch (final InvalidPointException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+            System.out.print(figure != null ? figure : " ");
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    private void printSeparator() {
+        System.out.println("~~~~~~~~~~~");
     }
 }
